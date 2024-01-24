0d134e603df288387f491cb5a88b9e6da425920d
Rajkumar Kasraveni <rajkumar@sentiasystems.com>
2024-01-24 19:02:23 +0530

    added ComicBooks Controller

---
+++ Controllers/ComicBooksController.cs
@@ -0,0 +1,12 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace NewComicGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
